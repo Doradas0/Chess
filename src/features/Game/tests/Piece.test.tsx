6034b0331333131a30f0518f03ebab545708765b
Doradas <daniel.malcolm@hotmail.co.uk>
2022-05-25 16:10:08 +0100

    add king

--- src/features/Game/tests/Piece.test.tsx
+++ src/features/Game/tests/Piece.test.tsx
@@ -1,6 +1,6 @@
-import '@testing-library/jest-dom'
-import {render, screen} from '@testing-library/react'
-import { Piece } from "../components/Piece"
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import { Piece } from "../components/Piece";
 
 it("renders a white pawn", () => {
 	render(<Piece />);
@@ -8,46 +8,56 @@ it("renders a white pawn", () => {
 });
 
 it("renders a black pawn", () => {
-	render(<Piece  black/>);
+	render(<Piece black />);
 	expect(screen.getByText("♟")).toBeInTheDocument();
 });
 
 it("renders a white knight", () => {
-	render(<Piece type="knight"/>);
+	render(<Piece type="knight" />);
 	expect(screen.getByText("♘")).toBeInTheDocument();
 });
 
 it("renders a black knight", () => {
-	render(<Piece type="knight" black/>);
+	render(<Piece type="knight" black />);
 	expect(screen.getByText("♞")).toBeInTheDocument();
 });
 
 it("renders a white bishop", () => {
-	render(<Piece type="bishop"/>);
+	render(<Piece type="bishop" />);
 	expect(screen.getByText("♗")).toBeInTheDocument();
 });
 
 it("renders a black bishop", () => {
-	render(<Piece type="bishop" black/>);
+	render(<Piece type="bishop" black />);
 	expect(screen.getByText("♝")).toBeInTheDocument();
 });
 
 it("renders a white rook", () => {
-	render(<Piece type="rook"/>);
+	render(<Piece type="rook" />);
 	expect(screen.getByText("♖")).toBeInTheDocument();
 });
 
 it("renders a black rook", () => {
-	render(<Piece type="rook" black/>);
+	render(<Piece type="rook" black />);
 	expect(screen.getByText("♜")).toBeInTheDocument();
 });
 
 it("renders a white queen", () => {
-	render(<Piece type="queen"/>);
+	render(<Piece type="queen" />);
 	expect(screen.getByText("♕")).toBeInTheDocument();
 });
 
 it("renders a black queen", () => {
-	render(<Piece type="queen" black/>);
+	render(<Piece type="queen" black />);
 	expect(screen.getByText("♛")).toBeInTheDocument();
+});
+
+it("renders a white king", () => {
+	render(<Piece type="king" />);
+	expect(screen.getByText("♔")).toBeInTheDocument();
+});
+
+it("renders a black king", () => {
+	render(<Piece type="king" black />);
+	expect(screen.getByText("♚")).toBeInTheDocument();
 });
--- src/features/Game/types.ts
+++ src/features/Game/types.ts
@@ -2,5 +2,5 @@ export type PieceType = {
 	//default is white
 	black?: boolean;
 	//default is pawn
-	type?: "knight" | "bishop" | "rook" | "queen";
+	type?: "knight" | "bishop" | "rook" | "queen" | "king";
 };
